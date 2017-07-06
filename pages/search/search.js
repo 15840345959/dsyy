0943d4c04fe9f31d2a5e6c5f83b769c39c887aec
gaolinghui <15904032696@163.com>
2017-07-06 17:39:15 +0800

    搜索页面

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,22 +1,31 @@
 var util = require('../../utils/util.js')
 var app=getApp()
 var vm = null
+var bar_id = 0
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     inputVal:[],
-    hotword: app.globalData.bookTypeArr,
     change:false,
     bookInfos:[],
-    search_book:[]
+    search_book:[],
+    hotword: getApp().globalData.bookTypeArr,
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     vm=this
+    bar_id = options.barid
+    if (bar_id =="undefined"||bar_id==null||bar_id==""){
+      bar_id=""
+    }
+    console.log("书吧传来的值："+bar_id)
+    if (bar_id == null) {
+      vm.inputTyping
+    }
   },
   /**
    * 生命周期函数--监听页面显示
@@ -37,31 +46,42 @@ Page({
 
     
   },
-
-  
-  //取消
-  hideInput:function(e){
-    this.setData({
+  hideInput:function(){
+    vm.setData({
       inputVal:"",
       change:false
     })
   },
-  search_bt: function (e){
-    console.log("search_book："+JSON.stringify(e))
-    var a = e.target.dataset.search
-    vm.output(a)
-    },
-  inputTyping: function (e) {
-    var a = e.detail.value
-    vm.output(a)
+  
+  search_bt:function(e){
+    vm.setData({
+      inputVal: e.target.dataset.search,
+    })
+    var param = {
+      type: e.target.dataset.search,
+      bar_id:bar_id
+    }
+    //如果为零属于重新加载
+    if (vm.data.bookInfos == "" || vm.data.bookInfos == null || vm.data.bookInfos == undefined) {
+      util.showLoading('加载图书');
+    }
+    util.getBookInfosByType(param, function (ret) {
+      console.log("输出：" + JSON.stringify(ret))
+      if (ret.data.code == "200") {
+        vm.setData({
+          bookInfos: ret.data.obj,
+          change: true
+        })
+      }
+    })
   },
-
-  output:function(a){
+  inputTyping:function(e){
       vm.setData({
-        inputVal: a,
+        inputVal: e.detail.value,
       })
       var param = {
-        title: a,
+        title: e.detail.value,
+        bar_id:bar_id
       }
       //如果为零属于重新加载
       if (vm.data.bookInfos == "" || vm.data.bookInfos == null || vm.data.bookInfos == undefined) {
