59dfd8cb6a42eb57c8c9089dae2283ce13f2aa77
TerryQi <15840345959@139.com>
2017-07-11 16:42:36 +0800

    完成支付

--- pages/payment/payment.js
+++ pages/payment/payment.js
@@ -1,5 +1,6 @@
 //logs.js
 var util = require('../../utils/util.js')
+var md5 = require('../../utils/md5.js')
 
 //index.js
 //获取应用实例
@@ -20,6 +21,37 @@ Page({
     }
     util.wxPrepay(param, function (ret) {
       console.log(JSON.stringify(ret))
+
+      var nonceStr = util.randomString()
+      var prepay_id = ret.data.obj.prepay_id
+      var timeStamp = util.createTimeStamp()
+
+
+      var stringSignTemp = "appId=&nonceStr=" + nonceStr + "&package=prepay_id=" + prepay_id + "&signType=MD5&timeStamp=" + timeStamp + "&key="
+      console.log("stringSignTemp:" + stringSignTemp)
+      var sign = md5.hexMD5(stringSignTemp).toUpperCase()
+
+      console.log("sign:" + sign)
+
+      wx.requestPayment({
+        timeStamp: timeStamp,
+        nonceStr: nonceStr,
+        package: "prepay_id=" + prepay_id,
+        signType: "MD5",
+        paySign: sign,
+        success: function (ret) {
+          // success  
+          console.log("sucess:" + JSON.stringify(ret))
+        },
+        fail: function (ret) {
+          // fail  
+          console.log("fail:" + JSON.stringify(ret))
+        },
+        complete: function () {
+          // complete  
+          console.log("complete:" + JSON.stringify(ret))
+        }
+      })
 
     })
   }
--- utils/util.js
+++ utils/util.js
@@ -192,357 +192,376 @@ function getBookInfosByType(param, successCallback, errorCallback) {
 //微信预下单
 function wxPrepay(param, successCallback, errorCallback) {
   wxRequest(SERVER_URL + '/APP/wxPrepay.do', param, "GET", successCallback, errorCallback);
+}
 
-  //根据用户id获取所属书吧列表
-  function getBarListByUserId(param, successCallback, errorCallback) {
-    wxRequest(SERVER_URL + '/APP/getBarListByUserId.do', param, "GET", successCallback, errorCallback);
-  }
+//根据用户id获取所属书吧列表
+function getBarListByUserId(param, successCallback, errorCallback) {
+  wxRequest(SERVER_URL + '/APP/getBarListByUserId.do', param, "GET", successCallback, errorCallback);
+}
 
-  //意见反馈
-  function createSuggestion(param, successCallback, errorCallback) {
-    wxRequest(SERVER_URL + '/APP/createSuggestion.do', param, "GET", successCallback, errorCallback);
-  }
+//意见反馈
+function createSuggestion(param, successCallback, errorCallback) {
+  wxRequest(SERVER_URL + '/APP/createSuggestion.do', param, "GET", successCallback, errorCallback);
+}
 
-  //更新用户信息
-  function updateUserInfo(param, successCallback, errorCallback) {
-    wxRequest(SERVER_URL + '/APP/updateUserInfo.do', param, "GET", successCallback, errorCallback);
-  }
+//更新用户信息
+function updateUserInfo(param, successCallback, errorCallback) {
+  wxRequest(SERVER_URL + '/APP/updateUserInfo.do', param, "GET", successCallback, errorCallback);
+}
 
-  //更新用户信息
-  function createBorrowCode(param, successCallback, errorCallback) {
-    wxRequest(SERVER_URL + '/APP/createBorrowCode.do', param, "GET", successCallback, errorCallback);
-  }
+//更新用户信息
+function createBorrowCode(param, successCallback, errorCallback) {
+  wxRequest(SERVER_URL + '/APP/createBorrowCode.do', param, "GET", successCallback, errorCallback);
+}
 
-  /////////基本方法///////////////////////////////////////////
-  //跳转到inputText页面
+/////////基本方法///////////////////////////////////////////
+//跳转到inputText页面
 
-  // var id = "nick_name"             输入框id
-  // var title = "用户昵称"             输入框标题
-  // var value = "我的昵称是变色龙"       输入框的默认值
-  // var length = 16                    输入框文字长度校验
-  // var tip = "好的昵称更容易让您记住您"   输入框下方提示
-  // var plach = "请输入昵称"          输入框提示
-  // var funName = "changeNickName"   上级页面的方法名
+// var id = "nick_name"             输入框id
+// var title = "用户昵称"             输入框标题
+// var value = "我的昵称是变色龙"       输入框的默认值
+// var length = 16                    输入框文字长度校验
+// var tip = "好的昵称更容易让您记住您"   输入框下方提示
+// var plach = "请输入昵称"          输入框提示
+// var funName = "changeNickName"   上级页面的方法名
 
-  function navigateToInput(param) {
-    console.log("navigateToInput" + JSON.stringify(param));
-    wx.navigateTo({
-      url: '/pages/inputText/inputText?jsonStr=' + JSON.stringify(param),
-    })
-  }
+function navigateToInput(param) {
+  console.log("navigateToInput" + JSON.stringify(param));
+  wx.navigateTo({
+    url: '/pages/inputText/inputText?jsonStr=' + JSON.stringify(param),
+  })
+}
 
-  //返回
-  function navigateBack(delta) {
-    wx.navigateBack({
-      delta: delta
-    })
-  }
+//返回
+function navigateBack(delta) {
+  wx.navigateBack({
+    delta: delta
+  })
+}
 
 
-  //数字处理
-  function formatNumber(n) {
-    n = n.toString()
-    return n[1] ? n : '0' + n
-  }
+//数字处理
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
 
-  //判断是否有空字符串
-  function judgeIsAnyNullStr() {
-    if (arguments.length > 0) {
-      for (var i = 0; i < arguments.length; i++) {
-        if (arguments[i] == null || arguments[i] == "" || arguments[i] == undefined || arguments[i] == "undefined" || arguments[i] == "未设置") {
-          return true;
-        }
+//判断是否有空字符串
+function judgeIsAnyNullStr() {
+  if (arguments.length > 0) {
+    for (var i = 0; i < arguments.length; i++) {
+      if (arguments[i] == null || arguments[i] == "" || arguments[i] == undefined || arguments[i] == "undefined" || arguments[i] == "未设置") {
+        return true;
       }
     }
-    return false;
   }
+  return false;
+}
 
-  //获取日期 2017-06-13
-  function getDateStr(str) {
-    if (judgeIsAnyNullStr(str)) {
-      return str
-    }
-    var pos = str.indexOf(' ');
-    if (pos < 0) {
-      return str
-    }
-    return str.substr(0, pos)
+//获取日期 2017-06-13
+function getDateStr(str) {
+  if (judgeIsAnyNullStr(str)) {
+    return str
   }
-
-  //格式化日期时间
-  function formatTime(date) {
-    var year = date.getFullYear()
-    var month = date.getMonth() + 1
-    var day = date.getDate()
-    var hour = date.getHours()
-    var minute = date.getMinutes()
-    var second = date.getSeconds()
-
-    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  var pos = str.indexOf(' ');
+  if (pos < 0) {
+    return str
   }
+  return str.substr(0, pos)
+}
+
+//格式化日期时间
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
 
 
-  //展示toast
-  function showToast(msg, img) {
-    console.log(img);
-    if (judgeIsAnyNullStr(img)) {
-      wx.showToast({
-        title: msg,
-        icon: 'success',
-        duration: 1500,
-      })
-    } else {
-      wx.showToast({
-        title: msg,
-        icon: 'success',
-        duration: 1500,
-        image: img
-      })
-    }
-  }
-
-  //展示modal
-  function showModal(title, content, confirmCallBack, cancelCallBack) {
-    wx.showModal({
-      title: title,
-      content: content,
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-          confirmCallBack(res)
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-          cancelCallBack(res)
-        }
-      }
-    })
-  }
-
-  //错误modal
-  function showErrorModal(msg) {
-    wx.showModal({
-      title: '调用失败',
-      content: msg,
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-        }
-      }
-    })
-  }
-
-  //展示loadding
-  function showLoading(msg) {
-    if (!wx.canIUse('showLoading')) {
-      return;
-    }
-    wx.showLoading({
+//展示toast
+function showToast(msg, img) {
+  console.log(img);
+  if (judgeIsAnyNullStr(img)) {
+    wx.showToast({
       title: msg,
+      icon: 'success',
+      duration: 1500,
+    })
+  } else {
+    wx.showToast({
+      title: msg,
+      icon: 'success',
+      duration: 1500,
+      image: img
     })
   }
+}
 
-  //隐藏loadding
-  function hideLoading() {
-    if (!wx.canIUse('hideLoading')) {
-      return;
+//展示modal
+function showModal(title, content, confirmCallBack, cancelCallBack) {
+  wx.showModal({
+    title: title,
+    content: content,
+    success: function (res) {
+      if (res.confirm) {
+        console.log('用户点击确定')
+        confirmCallBack(res)
+      } else if (res.cancel) {
+        console.log('用户点击取消')
+        cancelCallBack(res)
+      }
     }
-    wx.hideLoading();
+  })
+}
+
+//错误modal
+function showErrorModal(msg) {
+  wx.showModal({
+    title: '调用失败',
+    content: msg,
+    success: function (res) {
+      if (res.confirm) {
+        console.log('用户点击确定')
+      } else if (res.cancel) {
+        console.log('用户点击取消')
+      }
+    }
+  })
+}
+
+//展示loadding
+function showLoading(msg) {
+  if (!wx.canIUse('showLoading')) {
+    return;
   }
+  wx.showLoading({
+    title: msg,
+  })
+}
 
-
-  //优化字符串输出，如果str为空，则返回r_str
-  function conStr(str, r_str) {
-    if (judgeIsAnyNullStr(str)) {
-      return r_str;
-    }
-    return str;
+//隐藏loadding
+function hideLoading() {
+  if (!wx.canIUse('hideLoading')) {
+    return;
   }
+  wx.hideLoading();
+}
 
 
-  //鉴权相关
-  function convertEnNameToChiName(name) {
-    switch (name) {
-      case "":
-        return "";
-      case "":
-        return "";
-    }
-    return name;
+//优化字符串输出，如果str为空，则返回r_str
+function conStr(str, r_str) {
+  if (judgeIsAnyNullStr(str)) {
+    return r_str;
   }
+  return str;
+}
 
-  //判断是否为空图
-  function judgeIsNullImg(img_url) {
-    if (judgeIsAnyNullStr(img_url)) {
-      return true
-    }
-    if (img_url.indexOf('def.png') >= 0) {
-      return true
-    }
+
+//鉴权相关
+function convertEnNameToChiName(name) {
+  switch (name) {
+    case "":
+      return "";
+    case "":
+      return "";
+  }
+  return name;
+}
+
+//判断是否为空图
+function judgeIsNullImg(img_url) {
+  if (judgeIsAnyNullStr(img_url)) {
+    return true
+  }
+  if (img_url.indexOf('def.png') >= 0) {
+    return true
+  }
+  return false
+}
+
+//判断图书类型是否为空
+function judgeNoBookType(book_type) {
+  if (book_type.indexOf('其他') >= 0) {
+    return true
+  } else {
     return false
   }
-
-  //判断图书类型是否为空
-  function judgeNoBookType(book_type) {
-    if (book_type.indexOf('其他') >= 0) {
-      return true
-    } else {
-      return false
-    }
-  }
+}
 
 
-  function judgeIsAnyNullStrImp(obj) {
-    if (obj.length > 0) {
-      for (var i = 0; i < obj.length; i++) {
-        var value = obj[i].value;
-        var name = obj[i].name;
-        if (value == null || value == "" || value == undefined || value == "未设置") {
-          showToast("请设置" + convertEnNameToChiName(name), "../../images/close_icon.png");
-          return true;
-        }
+function judgeIsAnyNullStrImp(obj) {
+  if (obj.length > 0) {
+    for (var i = 0; i < obj.length; i++) {
+      var value = obj[i].value;
+      var name = obj[i].name;
+      if (value == null || value == "" || value == undefined || value == "未设置") {
+        showToast("请设置" + convertEnNameToChiName(name), "../../images/close_icon.png");
+        return true;
       }
     }
-    return false;
   }
+  return false;
+}
 
 
-  //是否还有本地图片
-  function isLocalImg(img) {
-    if (img.indexOf("wxfile") >= 0) {
-      return true;
-    }
-    return false;
+//是否还有本地图片
+function isLocalImg(img) {
+  if (img.indexOf("wxfile") >= 0) {
+    return true;
   }
+  return false;
+}
 
 
-  ///选择图片////////////////////////////////////////////////////////
-  function chooseImage(param, successCallBack, errorCallBack, completeCallBack) {
+///选择图片////////////////////////////////////////////////////////
+function chooseImage(param, successCallBack, errorCallBack, completeCallBack) {
 
-    //进行参数配置
-    if (judgeIsAnyNullStr(param.count)) {
-      param.count = 9
-    }
-    if (judgeIsAnyNullStr(param.sizeType)) {
-      param.sizeType = ['compressed']
-    }
-    if (judgeIsAnyNullStr(param.sourceType)) {
-      param.sourceType = ['album']
-    }
-    console.log("param :" + JSON.stringify(param))
+  //进行参数配置
+  if (judgeIsAnyNullStr(param.count)) {
+    param.count = 9
+  }
+  if (judgeIsAnyNullStr(param.sizeType)) {
+    param.sizeType = ['compressed']
+  }
+  if (judgeIsAnyNullStr(param.sourceType)) {
+    param.sourceType = ['album']
+  }
+  console.log("param :" + JSON.stringify(param))
 
-    wx.chooseImage({
-      sizeType: param.sizeType, // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: param.sourceType, // 可以指定来源是相册还是相机，默认二者都有
-      count: param.count,
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        console.log("wx.chooseImage success:" + JSON.stringify(res))
-        successCallBack(res)
-      },
-      fail: function (res) {
-        console.log("wx.chooseImage fail:" + JSON.stringify(res))
-        if (typeof errorCallBack == "function") {
-          errorCallBack(res)
-        }
-        errorCallBack(res);
-      },
-      complete: function (res) {
-        console.log("wx.chooseImage complete:" + JSON.stringify(res))
-        if (typeof completeCallBack == "function") {
-          completeCallBack(res)
-        }
+  wx.chooseImage({
+    sizeType: param.sizeType, // 可以指定是原图还是压缩图，默认二者都有
+    sourceType: param.sourceType, // 可以指定来源是相册还是相机，默认二者都有
+    count: param.count,
+    success: function (res) {
+      // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+      console.log("wx.chooseImage success:" + JSON.stringify(res))
+      successCallBack(res)
+    },
+    fail: function (res) {
+      console.log("wx.chooseImage fail:" + JSON.stringify(res))
+      if (typeof errorCallBack == "function") {
+        errorCallBack(res)
       }
-    })
-  }
-
-  function clone(myObj) {
-    if (typeof (myObj) != 'object') return myObj;
-    if (myObj == null) return myObj;
-
-    var myNewObj = new Object();
-
-    for (var i in myObj)
-      myNewObj[i] = clone(myObj[i]);
-
-    return myNewObj;
-  }
-
-  function getErrorMsg(error_code) {
-    switch (error_code) {
-      case "999":
-        return "调用失败"
+      errorCallBack(res);
+    },
+    complete: function (res) {
+      console.log("wx.chooseImage complete:" + JSON.stringify(res))
+      if (typeof completeCallBack == "function") {
+        completeCallBack(res)
+      }
     }
-    return "未知错误";
+  })
+}
+
+function clone(myObj) {
+  if (typeof (myObj) != 'object') return myObj;
+  if (myObj == null) return myObj;
+
+  var myNewObj = new Object();
+
+  for (var i in myObj)
+    myNewObj[i] = clone(myObj[i]);
+
+  return myNewObj;
+}
+
+function getErrorMsg(error_code) {
+  switch (error_code) {
+    case "999":
+      return "调用失败"
   }
+  return "未知错误";
+}
 
-  /*
-  ** randomWord 产生任意长度随机字母数字组合
-  ** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
-  ** xuanfeng 2014-08-28
-  ** 生成3-32位随机串：randomWord(true, 3, 32)
-  ** 生成43位随机串：randomWord(false, 43)
-  */
+/*
+** randomWord 产生任意长度随机字母数字组合
+** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
+** xuanfeng 2014-08-28
+** 生成3-32位随机串：randomWord(true, 3, 32)
+** 生成43位随机串：randomWord(false, 43)
+*/
 
-  function randomWord(randomFlag, min, max) {
-    var str = "",
-      range = min,
-      arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+function randomWord(randomFlag, min, max) {
+  var str = "",
+    range = min,
+    arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    // 随机产生
-    if (randomFlag) {
-      range = Math.round(Math.random() * (max - min)) + min;
-    }
-    for (var i = 0; i < range; i++) {
-      var pos = Math.round(Math.random() * (arr.length - 1));
-      str += arr[pos];
-    }
-    return str;
+  // 随机产生
+  if (randomFlag) {
+    range = Math.round(Math.random() * (max - min)) + min;
   }
-
-
-  module.exports = {
-    INDEX_PAGE: "/pages/index/index",
-    navigateToInput: navigateToInput,
-    navigateToLogin: navigateToLogin,
-    showLoading: showLoading,
-    hideLoading: hideLoading,
-    showToast: showToast,
-    showModal: showModal,
-    showErrorModal: showErrorModal,
-    navigateBack: navigateBack,
-    conStr: conStr,
-    clone: clone,
-    getDateStr: getDateStr,
-    randomWord: randomWord,
-    judgeIsAnyNullStr: judgeIsAnyNullStr,
-    judgeIsAnyNullStrImp: judgeIsAnyNullStrImp,
-    judgeNoBookType: judgeNoBookType,
-    isLocalImg: isLocalImg,
-    getImgRealUrl: getImgRealUrl,
-    qiniuUrlTool: qiniuUrlTool,
-    getRealImgUrl: getRealImgUrl,
-    judgeIsNullImg: judgeIsNullImg,
-    chooseImage: chooseImage,
-    getErrorMsg: getErrorMsg,
-    getOpenId: getOpenId,
-    getQnToken: getQnToken,
-    getADs: getADs,
-    login: login,
-    getBookInfoByISBN: getBookInfoByISBN,
-    updateBookInfo: updateBookInfo,
-    createBookObj: createBookObj,
-    getIndexPage: getIndexPage,
-    getBookObjByCon: getBookObjByCon,
-    getBarInfoByPos: getBarInfoByPos,
-    getBarPageByBarId: getBarPageByBarId,
-    getBookInfosByTitle: getBookInfosByTitle,
-    getBookInfosByType: getBookInfosByType,
-    wxPrepay: wxPrepay,
-    getBarListByUserId: getBarListByUserId,
-    createSuggestion: createSuggestion,
-    updateUserInfo: updateUserInfo,
-    createBorrowCode: createBorrowCode
+  for (var i = 0; i < range; i++) {
+    var pos = Math.round(Math.random() * (arr.length - 1));
+    str += arr[pos];
   }
+  return str;
+}
+
+
+/* 时间戳产生函数   */
+function createTimeStamp() {
+  return parseInt(new Date().getTime() / 1000) + ''
+}
+
+/* 随机数 */
+function randomString() {
+  var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+  /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  var maxPos = chars.length;
+  var pwd = '';
+  for (var i = 0; i < 32; i++) {
+    pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return pwd;
+}
+
+module.exports = {
+  INDEX_PAGE: "/pages/index/index",
+  navigateToInput: navigateToInput,
+  navigateToLogin: navigateToLogin,
+  showLoading: showLoading,
+  hideLoading: hideLoading,
+  showToast: showToast,
+  showModal: showModal,
+  showErrorModal: showErrorModal,
+  navigateBack: navigateBack,
+  conStr: conStr,
+  clone: clone,
+  getDateStr: getDateStr,
+  randomWord: randomWord,
+  judgeIsAnyNullStr: judgeIsAnyNullStr,
+  judgeIsAnyNullStrImp: judgeIsAnyNullStrImp,
+  judgeNoBookType: judgeNoBookType,
+  isLocalImg: isLocalImg,
+  getImgRealUrl: getImgRealUrl,
+  qiniuUrlTool: qiniuUrlTool,
+  getRealImgUrl: getRealImgUrl,
+  judgeIsNullImg: judgeIsNullImg,
+  chooseImage: chooseImage,
+  getErrorMsg: getErrorMsg,
+  getOpenId: getOpenId,
+  getQnToken: getQnToken,
+  getADs: getADs,
+  login: login,
+  getBookInfoByISBN: getBookInfoByISBN,
+  updateBookInfo: updateBookInfo,
+  createBookObj: createBookObj,
+  getIndexPage: getIndexPage,
+  getBookObjByCon: getBookObjByCon,
+  getBarInfoByPos: getBarInfoByPos,
+  getBarPageByBarId: getBarPageByBarId,
+  getBookInfosByTitle: getBookInfosByTitle,
+  getBookInfosByType: getBookInfosByType,
+  wxPrepay: wxPrepay,
+  getBarListByUserId: getBarListByUserId,
+  createSuggestion: createSuggestion,
+  updateUserInfo: updateUserInfo,
+  createBorrowCode: createBorrowCode,
+  createTimeStamp: createTimeStamp,
+  randomString: randomString
 }
